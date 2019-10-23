4a44c2b2ad726f4ae03e5d3af288fcfe8f3c6757
theDan06 <thedan06@gmail.com>
2019-10-23 20:37:24 +0300

    Moved function under TODO instruction

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -25,6 +25,9 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
+    // ***
     public void createMapIntent(View view){
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
 
@@ -35,10 +38,6 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
-
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
