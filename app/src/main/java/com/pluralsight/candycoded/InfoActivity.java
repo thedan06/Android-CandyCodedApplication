01feb2c15d09931123f2d802c0feb276ad5cc690
theDan06 <thedan06@gmail.com>
2019-10-23 21:12:29 +0300

    Start the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -28,7 +28,7 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
-    public void createMapIntent(View view){
+    public void createMapIntent(View view) {
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
 
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
@@ -42,4 +42,13 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+
+        Uri uri = Uri.parse("tel:0123456789");
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(uri);
+
+        startActivity(intent);
+    }
 }
